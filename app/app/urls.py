c998bce44be992878b877dba627ce7c0623331bf
Ragnar Johannesen <ragnar123@gmail.com>
2015-11-30 17:37:39 +0100

    basic http stuff.

--- app/app/urls.py
+++ app/app/urls.py
@@ -2,10 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^info/', 'weather_station.views.detail'),
+    url(r'^info/([0-9]{4})', 'weather_station.views.info'), 
     url(r'/*', 'weather_station.views.index'),
 )
