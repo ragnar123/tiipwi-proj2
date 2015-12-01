ef5c8f2ef9205853eed8a52d63e9be78692eaf1b
Ragnar Johannesen <ragnar123@gmail.com>
2015-12-01 17:34:50 +0100

    tried to get auth working

--- app/weather_station/authentication.py
+++ app/weather_station/authentication.py
@@ -6,25 +6,26 @@ import random
 
 global DEFAULT_SIZE=6
 
-def psw_generator(psw_size, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(psw_size))
+class Authentication:
+    def psw_generator(psw_size, chars=string.ascii_uppercase + string.digits):
+        return ''.join(random.choice(chars) for _ in range(psw_size))
 
-def createNewUser(device_mac):
-    user = User.objects.create_user(device_mace, mail=None, password=None)
-    user.set_password(psw_generator(DEFAULT_SIZE))
-    user.groups.add(authenticated_sensor_group)
+    def createNewUser(device_mac):
+        user = User.objects.create_user(device_mac, mail=None, password=None)
+        user.set_password(psw_generator(DEFAULT_SIZE))
+        user.groups.add(authenticated_sensor_group)
 
 
-def ifAuthenticatedAddEntry(user_id, raw_password)
-    try:
-        node = SensorNode.objects.get(sensor_id=user_id)
-    except SensorNode.DoesNotExist:
-        return HttpResponse("Unable to find node");
-    except SensorNode.MultipleObjectsReturned:
-        return HttpResponse("her er heilt galid!");
+    def ifAuthenticatedAddEntry(user_id, raw_password)
+        try:
+            node = SensorNode.objects.get(sensor_id=user_id)
+        except SensorNode.DoesNotExist:
+            return HttpResponse("Unable to find node");
+        except SensorNode.MultipleObjectsReturned:
+            return HttpResponse("her er heilt galid!");
 
-    if check_password(raw_password):
-        new_entry = SensorReading(node_id=user_id , timestamp= str(datetime.datetime.now())
-        new_entry.save()
-    else:
-        return "sorry wrong password"
+        if check_password(raw_password):
+            new_entry = SensorReading(node_id=user_id , timestamp= str(datetime.datetime.now())
+            new_entry.save()
+        else:
+            return "sorry wrong password"
--- app/weather_station/views.py
+++ app/weather_station/views.py
@@ -48,9 +48,49 @@ def list(request):
 
 
 
+from django.contrib.auth.models import User
+from weather_station.models import SensorNode
+import datetime
+import string
+import random
+
+DEFAULT_SIZE=6
+
+def psw_generator(psw_size, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(psw_size))
+
+def createNewUser(device_mac):
+    user = User.objects.create_user(device_mac, email=None, password=None)
+    user.set_password(psw_generator(DEFAULT_SIZE))
+    #user.groups.add(authenticated_sensor_group)
+
+
+def ifAuthenticatedAddEntry(user_id, raw_password):
+    try:
+        node = SensorNode.objects.get(sensor_id=user_id)
+    except SensorNode.DoesNotExist:
+        return HttpResponse("Unable to find node");
+    except SensorNode.MultipleObjectsReturned:
+        return HttpResponse("her er heilt galid!");
+
+    if check_password(raw_password):
+        new_entry = SensorReading(node_id=user_id , timestamp= str(datetime.datetime.now()))
+        new_entry.save()
+    else:
+        return "sorry wrong password"
+
+
+
+
+
 def signup(request):
     resp = "The idea is that this will return a new nodeid & key."
+
+    name = "asdf_unique+1"
+    createNewUser(name)
+
     return HttpResponse(resp)
+
 
 
 def put_reading(request, node_id, type, value):
