2b9cc2172e4bfef9635848bfc4f588fc30dd8957
Ragnar Johannesen <ragnar123@gmail.com>
2015-12-09 14:34:47 +0100

    trying to get user auth to work

--- app/weather_station/authentication.py
+++ app/weather_station/authentication.py
@@ -14,20 +14,31 @@ class Authentication:
         user = User.objects.create_user(device_mac, mail=None, password=None)
         user.set_password(psw_generator(DEFAULT_SIZE))
         user.groups.add(authenticated_sensor_group)
+        user.save()
 
 
     def ifAuthenticatedAddEntry(self, user_id, raw_password):
         # Distinction between User and SensorNode. ? (/signup produces a username.)
         # Sensor_ID is now 6digit string (was 4 digit numeric) (migration ok)
+        print user_id
         try:
-            node = SensorNode.objects.get(sensor_id=user_id)
-        except SensorNode.DoesNotExist:
-            # Throw exception
-            print "fail"
+            user = User.objects.get(username=user_id)
+            print ("CHECKING USER " + user_id + ". IF PW " + raw_password + " is valid..");
+            if user.check_password(raw_password):
+                print "We were able to authenticate you."
+                print "need to initialize a SensorNode with id as yours"
+                sn = SensorNode.objects.get(sensor_id = user_id)
+                if not sn:
+                    print "not found. creating"
+                    sn = SensorNode(sensor_id = user_id)
+                    sn.save();
 
-        user = User(username=user_id)
-        if user.check_password(raw_password):
-            new_entry = SensorReading(node_id=user_id , timestamp= str(datetime.datetime.now()))
-            new_entry.save()
-        else:
-            return "sorry wrong password"
+                return True
+
+            else:
+                raise Exception('User not known.')
+        except SensorNode.DoesNotExist:
+            print "fail"
+        except Exception as failure:
+            print "wrong PW" + str(failure)
+            return False;
