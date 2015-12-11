3651d7f9cfe681d327d3f4e077605810e51ad9bd
Ragnar Johannesen <ragnar123@gmail.com>
2015-12-11 15:01:10 +0100

    Updated test

--- tests_sensornode.py
+++ tests_sensornode.py
@@ -8,7 +8,7 @@ class SensorNodeTestCase(TestCase):
     def test_verify_sensor_node(self):
         """Checking that the sensor readings are empty for new SensorNode"""
         sn1 = SensorNode.objects.get(sensor_id="abcdef")
-        self.assertEqual(sn1.getNumberOfReadings(), 0)
+        self.assertEqual(sn1.get_number_of_readings(), 0)
 
         self.assertEqual(sn1.sensor_id, 'abcdef')
 
